f2f865f7a3454343924772bf1abe7c32487e0458
Fiki <cerebrumedukanesia@gmail.com>
2021-01-29 17:26:15 +0700

    add till half of post controller

---
+++ api/controllers/posts_controller.go
@@ -0,0 +1,68 @@
+package controllers
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"strconv"
+
+	"github.com/fikifh/fullstack/api/auth"
+	"github.com/fikifh/fullstack/api/models"
+	"github.com/fikifh/fullstack/api/responses"
+	"github.com/fikifh/fullstack/api/utils/formaterror"
+	"github.com/gorilla/mux"
+)
+
+func (server *Server) CreatedPost(w http.ResponseWriter, r *http.Request) {
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		responses.ERROR(w, http.StatusUnprocessableEntity, err)
+	}
+	post := models.Post{}
+	err = json.Unmarshal(body, &post)
+	if err != nil {
+		responses.ERROR(w, http.StatusUnprocessableEntity, err)
+		return
+	}
+	post.Prepare()
+	err = post.Validate()
+	if err != nil {
+		responses.ERROR(w, http.StatusUnprocessableEntity, err)
+		return
+	}
+	uid, err := auth.ExtractTokenID(r)
+	if err != nil {
+		responses.ERROR(w, http.StatusUnauthorized, errors.New("Unauthorized"))
+		return
+	}
+	if uid != post.AuthorID {
+		responses.ERROR(w, http.StatusUnauthorized, err)
+		return
+	}
+	postCreated, err := post.SavePost(server.DB)
+	if err != nil {
+		formatedError := formaterror.FormatError(err.Error())
+		responses.ERROR(w, http.StatusInternalServerError, formatedError)
+		return
+	}
+	w.Header().Set("Location", fmt.Sprintf("%s%s/%d", r.Host, r.URL.Path, postCreated.ID))
+	responses.JSON(w, http.StatusCreated, postCreated)
+}
+
+func (server *Server) GetPost(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	pid, err := strconv.ParseUint(vars["id"], 10, 64)
+	if err != nil {
+		responses.ERROR(w, http.StatusBadRequest, err)
+	}
+	post := models.Post{}
+
+	postReceived, err := post.FindPostByID(server.DB, pid)
+	if err != nil {
+		responses.ERROR(w, http.StatusInternalServerError, err)
+		return
+	}
+	responses.JSON(w, http.StatusOK, postReceived)
+}
